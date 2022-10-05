9cbfe230b1f77a9437b0ae06728d975eca897f64
David Horn <dhorn@brex.com>
2022-10-05 11:26:40 -0700

    added fuzzyscore algo

--- comparison_algorithm.rb
+++ comparison_algorithm.rb
@@ -27,4 +27,32 @@ module ComparisonAlgorithm
       word_a.jarowinkler_similar(word_b).round(ROUND)
     end
   end
+
+  module FuzzyScore
+    def self.similarity(word_a, word_b)
+      a = word_a.downcase
+      b = word_b.downcase
+      score = 0
+      termIndex = 0
+      previousMatchingCharacterIndex = -1
+      b.split("").each do |queryChar|
+        termCharacterMatchFound = false
+        while termIndex < a.length && !termCharacterMatchFound
+          termChar = a[termIndex]
+          if queryChar == termChar
+            score = score + 1
+            if previousMatchingCharacterIndex+1 == termIndex
+              score = score + 2
+            end
+            previousMatchingCharacterIndex = termIndex
+            termCharacterMatchFound = true
+          end
+
+          termIndex = termIndex + 1
+        end
+      end
+
+      (score*2.0/(b.length+a.length)).round(3)
+    end
+  end
 end
